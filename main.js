0a03ec1dceb5d6fd2ddee57a62fcf35bb4e35435
Deepaliatgithub <77678328+Deepaliatgithub@users.noreply.github.com>
2021-08-05 14:44:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+
+   var nameone = "Deepali";
+  console.log(nameone);
+  
+  var nametwo = "Mohan";
+  console.log(nametwo);
+  var fullname = nameone.concat(nametwo);
+  console.log(fullname);
+
+
+  var numbers = ["27","22","98","78",];
+    console.log(numbers);
+    
+    var maximum_number = Math.max.apply(Math , numbers);
+    console.log(maximum_number);
+
+  var minimum_number = Math.min.apply(Math , numbers);
+    console.log(minimum_number);
+    
+    
+ 
+
+  
